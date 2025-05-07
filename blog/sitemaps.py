24fd6fcb5a77a94eb6c6cf4bd51d7617f7ed4bd3
luifervillalba <villalba.luifer@gmail.com>
2025-05-06 23:13:18 -0300

    Tests updated.

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,6 +1,7 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
 from django.urls import reverse
+from taggit.models import Tag
 
 
 class PostSitemap(Sitemap):
@@ -12,3 +13,19 @@ class PostSitemap(Sitemap):
     
     def lastmod(self, obj):
         return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = 'daily'
+    priority = 0.8
+
+    def items(self):
+        # Only include tags that are actually used in published posts
+        return Tag.objects.filter(post__status=Post.Status.PUBLISHED).distinct()
+
+    def location(self, obj):
+        return reverse('blog:post_list_by_tag', args=[obj.slug])
+
+    def lastmod(self, obj):
+        # Get the most recent post for this tag
+        return Post.published.filter(tags=obj).latest('updated').updated
