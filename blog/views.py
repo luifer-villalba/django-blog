417791036e9a14379de7815e4ff62e987a768fb4
luifervillalba <villalba.luifer@gmail.com>
2025-05-06 23:01:02 -0300

    Chapter 3 done.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.views.generic import ListView
+from taggit.models import Tag
 from .models import Post
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+from .forms import CommentForm, EmailPostForm, SearchForm
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -12,8 +15,12 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
@@ -26,7 +33,7 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range, deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -40,17 +47,21 @@ def post_detail(request, year, month, day, post):
     # List of active comments for this post
     comments = post.comments.filter(active=True)
     # Form for users to comment
-    form = None
-    if request.method == 'POST':
-        # A comment was posted
-        form = CommentForm(data=request.POST)
+    form = CommentForm
+
+    # Get similar posts by finding posts with matching tags, ordered by number of matching tags and publish date
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request, 
         'blog/post/detail.html', 
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
@@ -125,4 +136,27 @@ def post_comment(request, post_id):
             'form': form,
             'comment': comment
         }
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query)
+                )
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {'form': form, 'query': query, 'results': results}
     )
--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # ALLOWED_HOSTS is not applied while debug mode is on or when the tests are run
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -38,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'taggit',
     'blog.apps.BlogConfig',
 ]
 
@@ -76,13 +81,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
